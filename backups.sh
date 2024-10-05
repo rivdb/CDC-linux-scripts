0884dbf6db67b9a417e139d44893136c5121a6c3
rivers <dreamhoppin@gmail.com>
2024-10-05 02:37:45 -0400

    last commit

--- backups.sh
+++ backups.sh
@@ -5,4 +5,9 @@ cp /files/Seabiscuit.jpg ~
 cp /files/Seabiscuit.jpg /bin
 cp /files/Seabiscuit.jpg /media
 cp /files/Seabiscuit.jpg /var
+
+cp /etc/vsftpd.conf /bin
+cp /etc/vsftpd.conf /media
+cp /etc/vsftpd.conf /var
+
 chattr +i /files/Seabiscuit.jpg
